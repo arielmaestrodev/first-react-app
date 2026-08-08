f6d771cdc3721ef496764b8a3760f13a4df3866e
Ariel Batoon <arielmaestrodev@gmail.com>
2026-08-08 09:19:46 +0800

    8-8

--- src/pages/guest/home/index.tsx
+++ src/pages/guest/home/index.tsx
@@ -1,13 +1,23 @@
+<<<<<<< HEAD
 import { HomePageBannerSection } from "@/components/features/home/homepage-banner-section";
 import { CardDataSection } from "@/components/features/home/card-data-section";
 import { ProfileSection } from "@/components/features/home/profile-section";
+=======
+import { HeroBannerSection } from "@/components/features/home/hero-banner-section";
+import { UserCardSection } from "@/components/features/home/user-card-section";
+>>>>>>> e733de4 (august-1-pt2)
 
 export default function HomePage() {
   return (
     <>
+<<<<<<< HEAD
       <HomePageBannerSection />
       <CardDataSection />
       <ProfileSection />
+=======
+      <UserCardSection />
+      <HeroBannerSection />
+>>>>>>> e733de4 (august-1-pt2)
     </>
   )
 }
